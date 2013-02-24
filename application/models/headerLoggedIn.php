b8cbd52a37e1e96f76472f5c3ad93c0ab395e064
Rebekah David <mvemjsunp@github.com>
2013-02-24 17:58:32 -0500

    added script to retrieve the test dataset

--- application/models/headerLoggedIn.php
+++ application/models/headerLoggedIn.php
@@ -11,9 +11,13 @@ $getName = mysql_query($queryGetName, $openSpectra) or die(mysql_error());
 $row_GetName = mysql_fetch_assoc($getName);
 $USER_NAME = $row_GetName['FIRSTNAME'] . "&nbsp;" . $row_GetName['LASTNAME'];
 $CC_USER_NAME = mb_convert_case($USER_NAME, MB_CASE_TITLE, "UTF-8");
-$ORIGFILE_URL = 
 $USER_USERNAME = $row_GetName['USERNAME'];
 $USER_INSTITUTION = $row_GetName['INSTITUTION'];
+
+$queryGetData = "SELECT * FROM DATASETS";
+$getData = mysql_query($queryGetData, $openSpectra) or die(mysql_error());
+$row_GetData = mysql_fetch_assoc($getData);
+$ORIGFILE_URL = $row_GetData['ORIGFILE_URL'];
 
 
 if ($userloginid === -1) {
