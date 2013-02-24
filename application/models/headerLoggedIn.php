f99b88b958d1f27f0c84caa9e98719c810ecf145
Rebekah David <mvemjsunp@github.com>
2013-02-24 16:58:04 -0500

    cleaned up error handling messages hopefully

--- application/models/headerLoggedIn.php
+++ application/models/headerLoggedIn.php
@@ -6,11 +6,13 @@ $USER_USERNAME = NULL;
 $userloginid = $usermgr->sessionLoggedIn(session_id());
 
 mysql_select_db($database_openSpectra, $openSpectra);
-$queryGetName = "SELECT LASTNAME, FIRSTNAME FROM USER WHERE USER_ID='" . $userloginid . "' LIMIT 0, 1";
+$queryGetName = "SELECT * FROM USER WHERE USER_ID='" . $userloginid . "' LIMIT 0, 1";
 $getName = mysql_query($queryGetName, $openSpectra) or die(mysql_error());
 $row_GetName = mysql_fetch_assoc($getName);
-$USER_NAME = "&nbsp" . $row_GetName['FIRSTNAME'] . "&nbsp;" . $row_GetName['LASTNAME'];
+$USER_NAME = $row_GetName['FIRSTNAME'] . "&nbsp;" . $row_GetName['LASTNAME'];
 $CC_USER_NAME = mb_convert_case($USER_NAME, MB_CASE_TITLE, "UTF-8");
+$USER_USERNAME = $row_GetName['USERNAME'];
+$USER_INSTITUTION = $row_GetName['INSTITUTION'];
 
 
 if ($userloginid === -1) {
