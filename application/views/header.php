48016eba9fdf0db01f53d53a46d101f77125b251
Emily Watson <emily.kathryn.watson@gmail.com>
2013-04-14 11:03:45 -0400

    added modals.php and header.php

---
+++ application/views/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Workspace</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+
+    <!-- styles -->
+    
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,600,700' rel='stylesheet' type='text/css'>
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>  
+    <link href="css/font-awesome.min.css" rel="stylesheet" />
+    <style type="text/css">
+
+      body {
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+    </style>
+     <link href="css/bootstrap-responsive.css" rel="stylesheet"/>
+     <link href="css/bootstrap-fileupload.min.css" rel="stylesheet"/>
+     <link href="css/bootstrap-scroll-modal.css" rel="stylesheet" />
+     <link href="css/oxygen-stylesheet.css" rel="stylesheet" />
+     <link href="css/styles.css" rel="stylesheet"/>
+     <link href="css/datepicker.css" rel="stylesheet" />
+
+     
+    
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="ico/apple-touch-icon-144-precomposed.png"/>
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="ico/apple-touch-icon-114-precomposed.png"/>
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="ico/apple-touch-icon-72-precomposed.png"/>
+    <link rel="apple-touch-icon-precomposed" href="ico/apple-touch-icon-57-precomposed.png"/>
+    <link rel="shortcut icon" href="ico/favicon.png"/>
+
+
+  </head>
