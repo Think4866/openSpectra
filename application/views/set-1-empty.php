95bc4a49a57c6e598c3466499edfb55032e698e1
Emily Watson <emily.kathryn.watson@gmail.com>
2013-04-14 10:53:29 -0400

    added php templates as includes for all loading windows, added scroll bar for set lists

---
+++ application/views/set-1-empty.php
@@ -0,0 +1,10 @@
+            <div class="set-1-empty span12">
+              <section class="load-another span12">
+                <div id="add-another-btns" class="btn-toolbar pagination-centered">
+                  <div class="">
+                    <label>Add Another Set</label>
+                    
+                  </div>
+                </div>
+              </section>
+            </div>  <!-- end set one empty -->
---
+++ application/views/start-screen.php
@@ -0,0 +1,9 @@
+            <div class="span12">
+              <section class="span12">
+                <div class="welcome-screen">
+
+                	<h1>Welcome to SpectraView</h1>
+
+                </div>
+              </section>
+            </div>  <!-- end start screen -->
