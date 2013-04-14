7fcdbc9b8710ef3e26a1b46d503b1218ad382230
Emily Watson <emily.kathryn.watson@gmail.com>
2013-04-14 13:11:25 -0400

    added start screen content

--- application/views/start-screen.php
+++ application/views/start-screen.php
@@ -2,7 +2,16 @@
               <section class="span12">
                 <div class="welcome-screen">
 
-                	<h1>Welcome to SpectraView</h1>
+                	<h1>Hello, <?php echo $CC_USER_NAME; ?></h1>
+                		<p class="intro-message span9">Welcome to your workspace. Utilize the options contained in the sidebars to your left and right to manage your data sets. Once two sets are loaded, utilize the lower toolbar to compare them.</p>
+                		<p>To begin viewing and comparing data:</p>
+                		<ul class="welcome-options span9">
+                			<li><i class="icon-arrow-left"></i>upload a data set</li>
+                			<p>OR</p>
+                			<li>choose from the available sets in the browsing sidebar<i class="icon-arrow-right"></i></li>
+                		</ul>  
+
+                		<!-- <p class="no-show-again">do not show welcome screen again</p> -->              	  
 
                 </div>
               </section>
