c89c2d36f50c04af163b3f09b822da99347e7e83
Nathan Bennett <ntbnnt@gmail.com>
2013-04-24 05:57:27 -0400

    processData script updated to connect to s3 and interop with php

--- scripts/processData.py
+++ scripts/processData.py
@@ -3,25 +3,30 @@ __purpose__ = 'Process files uploaded by the user.'
 
 from xml.etree import ElementTree
 import sys
-import boto
+import os
+import boto.s3
 from boto.s3.key import Key
 from time import gmtime, strftime
 
 isConnected = boto.connect_s3()
 
 #load args from upload_datasets.php, argv[0] is the name of the script and args passed here increment in the passed order
-#inputFile = argv[1]
-#incValue = argv[2]
+inputFile = sys.argv[1]
+incValue = sys.argv[2]
 
-inputFile = "../application/models/data/a11001.ws5" #for testing
+#inputFile = "../application/models/data/a11001.ws5" #for testing
+
+fileName = "../application/models/data/output.xml" #for testing
+
+outputFile = open(fileName, "w") #for testing
 
 #reads the WS5 file and takes out the important stuff
 def processWS5Data(inputFile):
     if isConnected:
-        bucket = "spectraview-xml-data"
-        s3Connection = Key(bucket)
-        s3Connection.key = inputFile
-        finalS3URL = ""
+        bucket = bucket = isConnected.get_bucket('spectraview-xml-data')
+        dataHeader = "<mugl><plot><renderer type=\"pointline\"><option name=\"linecolor\" value=\"black\"/><option name=\"pointcolor\" value=\"blue\"/><option name=\"pointsize\" value=\"1.5\"/></renderer></plot><data><values>"
+        outputFile.write("%s"% dataHeader)
+        dataFooter = "</values></data></mugl>"
         print "I've connected to Amazon S3 at %s" % strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
         with open(inputFile, 'rt') as ourFile:
             tree = ElementTree.parse(ourFile)
@@ -34,9 +39,16 @@ def processWS5Data(inputFile):
                 channelFinal = str(index)
                 channelFinal = channelFinal + (",")
                 dataFinal = str(element)
-                finalFormatedData = channelFinal+(" ")+dataFinal
-                s3Connection.set_contents_from_string('%s',encrypt_key=False) % finalFormatedData
-                #print str(finalFormatedData)
+                finalFormatedData = channelFinal+(" ")+dataFinal+("\n")
+                #s3Connection.set_contents_from_string('%s') % finalFormatedData
+                outputFile.write("%s"% finalFormatedData)
+        outputFile.write("%s"% dataFooter)
+        outputFile.close()
+        k = Key(bucket)
+        k.key = "%s" % fileName
+        k.set_contents_from_filename(fileName)
+        finalS3URL = sys.argv[1]
+        os.system("/usr/bin/php /../models/new_dataset_add.php %s %d") % finalS3URL, incValue
 
 #Checks the file name and runs the appropriate function
 def checkFileName(inputFile):
